b581298b8710391ff47b6591aafaa5310762ef16
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-08 12:44:43 +0530

    ci(linkcheck): ignore opennetworking.org temporarily to avoid cert-related failures
    Signed-off-by: Arnav Kapoor <kapoorarnav43@gmail.com>

--- conf.py
+++ conf.py
@@ -257,6 +257,10 @@ linkcheck_ignore = [
     r'https://jenkins.aetherproject.org/.*',
     r'https://gerrit.opencord.org/.*',
     r'https://wiki.opennetworking.org/.*',
+    # Opennetworking.org currently returns certificate verification errors in CI
+    # (expired/invalid cert). Ignore their domain temporarily to avoid linkcheck
+    # failures. Replace or re-enable checks when upstream fixes their cert.
+    r'https?://opennetworking\.org/.*',
 ]
 
 linkcheck_timeout = 3
